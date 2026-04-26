207a759ddebdc4fc844383b0ea6ee38b04c49827
Luis Sagnay <sagnayluis99@gmail.com>
2026-04-26 17:48:26 -0500

    feat: add footer and navigation components with styles and HTML structure

---
+++ url/src/app/components/layout/footer/footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IconComponent } from '../../ui/icon/icon';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [IconComponent],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class FooterComponent {}

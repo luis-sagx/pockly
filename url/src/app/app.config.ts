3b401d99b4246fac276ed4e8e23bef9c2e78eb94
Luis Sagnay <sagnayluis99@gmail.com>
2026-06-12 12:09:34 -0500

    feat(url): add PWA offline support

--- url/src/app/app.config.ts
+++ url/src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 
 import { routes } from './app.routes';
 
@@ -9,5 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideClientHydration(withEventReplay()),
+    provideServiceWorker('ngsw-worker.js', { enabled: !isDevMode() }),
   ]
 };

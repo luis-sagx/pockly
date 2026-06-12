c2cdd814f043108d72cc3acb465e0b92fbd2921a
Luis Sagnay <sagnayluis99@gmail.com>
2026-06-12 11:05:01 -0500

    fix: resolve remaining SEO/routing/test warnings

--- json/src/app/app.spec.ts
+++ json/src/app/app.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { App } from './app';
 import { LanguageService } from './services/language.service';
 
@@ -6,7 +7,7 @@ describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [LanguageService],
+      providers: [provideRouter([]), LanguageService],
     }).compileComponents();
   });
 
